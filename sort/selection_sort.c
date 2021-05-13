9ef34d3982191fbc7f3409639e55c15ea66e3a78
Sara Vieira <s.cvieira@sapo.pt>
2021-05-13 17:38:43 +0100

    selection sort

---
+++ sort/selection_sort.c
@@ -0,0 +1,28 @@
+int	*selection_sort(int *arr, int length)
+{
+	int	i;
+	int	j;
+	int	min;
+	int	tmp;
+
+	i = 0;
+	while (i < length)
+	{
+		j = i + 1;
+		min = i;
+		while (j < length)
+		{
+			if (arr[j] < arr[min])
+				min = j;
+			j++;
+		}
+		if (min != i)
+		{
+			tmp = arr[i];
+			arr[i] = arr[min];
+			arr[min] = tmp;
+		}
+		i++;
+	}
+	return (arr);
+}
--- sort/test.c
+++ sort/test.c
@@ -2,23 +2,36 @@
 #include <time.h>
 
 int	*bubble_sort(int *arr, int length);
+int	*selection_sort(int *arr, int length);
 
 int main(void)
 {
 	int	i = 0;
 	int	length = 16;
-	int	arr[] = {3, 70, 45, 1, 9, 26, 59, 33, 82, 2, 15, 45, 10, 100, 0, 91};
-	int	*bubble_sorted;
+	int	arr1[] = {3, 70, 45, 1, 9, 26, 59, 33, 82, 2, 15, 45, 10, 100, 0, 91};
+	int	arr2[] = {3, 70, 45, 1, 9, 26, 59, 33, 82, 2, 15, 45, 10, 100, 0, 91};
 	time_t	before;
 	time_t	after;
 
 	before = time(NULL);
-	bubble_sorted = bubble_sort(arr, length);
+	bubble_sort(arr1, length);
 	after = time(NULL);
 	printf("Bubble Sort took %f seconds to sort\n", (float)(after - before));
 	while (i < length)
 	{
-		printf("%d ", arr[i]);
+		printf("%d ", arr1[i]);
+		i++;
+	}
+	printf("\n");
+
+	before = time(NULL);
+	selection_sort(arr2, length);
+	after = time(NULL);
+	printf("Selection Sort took %f seconds to sort\n", (float)(after - before));
+	i = 0;
+	while (i < length)
+	{
+		printf("%d ", arr2[i]);
 		i++;
 	}
 	printf("\n");
