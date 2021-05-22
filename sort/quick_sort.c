a41387bbdece155ebfeb48b08f26bb20749ae920
Sara Vieira <s.cvieira@sapo.pt>
2021-05-22 19:34:07 +0100

    improve readability

--- sort/quick_sort.c
+++ sort/quick_sort.c
@@ -7,9 +7,8 @@ void	quick_sort(int *arr, int start, int end)
 
 	// halting condition
 	if (start >= end)
-			return ;
+		return ;
 	
-	// to iterate over the array:
 	i = start;
 	// to know where the pivot will be
 	future_pivot_i = start;
@@ -19,8 +18,8 @@ void	quick_sort(int *arr, int start, int end)
 	while (i < end)
 	{
 		// if a number is lower than the pivot:
-		// place it on the left (swap it)
-		// move future location of the pivot forward
+		// - place it on the left (swap it)
+		// - move future location of the pivot forward
 		if (arr[i] < pivot)
 		{
 			tmp = arr[i];
@@ -30,14 +29,13 @@ void	quick_sort(int *arr, int start, int end)
 		}
 		i++;
 	}
-	// at this point our array is organised in this way:
-	// [lower numbers, higher numbers, pivot]
-	// let's place the pivot in between
-	// future_pivot_i is pointing at the right position
+
+	// at this point our array is organised in this way: [lower nbs, higher nbs, pivot]
+	// let's put the pivot in between - future_pivot_i is pointing at the right place
 	arr[end] = arr[future_pivot_i];	
 	arr[future_pivot_i] = pivot;
 
-	// call function recursively for lower and higher blocks
+	// call function recursively for left and right sub-arrays
 	quick_sort(arr, start, future_pivot_i - 1);
 	quick_sort(arr, future_pivot_i + 1, end);
 }
