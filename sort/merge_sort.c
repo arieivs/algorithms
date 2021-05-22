e0c114e05c53940388f2d99737247268079c9a63
Sara Vieira <s.cvieira@sapo.pt>
2021-05-22 19:52:51 +0100

    improved merge sort, tmp array is now created in the merge function

--- sort/merge_sort.c
+++ sort/merge_sort.c
@@ -1,18 +1,20 @@
 #include <stdlib.h>
 
-// divide the array in two and call merge on the result of dividing them again
-// in this way it will first divide the array in single elements
-// and then merge then in an ordered manner
+// divide the array in two and call merge on both halves 
+// so it will first divide the array in single elements and then merge them in an ordered manner
 
-void	merge(int *arr, int start, int middle, int end, int *tmp_arr)
+void	merge(int *arr, int start, int middle, int end)
 {
 	int	i;
 	int	j;
 	int	k;
+	int	*tmp_arr;
 
 	i = start;
 	j = middle + 1;
-	k = start;
+	k = 0;
+	tmp_arr = (int *)malloc(sizeof(int) * (end + 1 - start));
+	
 	// copy the smallest number
 	while (i <= middle && j <= end)
 	{
@@ -28,35 +30,29 @@ void	merge(int *arr, int start, int middle, int end, int *tmp_arr)
 	// in case the second half is bigger
 	while (j <= end)
 		tmp_arr[k++] = arr[j++];
+	
 	// update array with the more sorted temporary version
 	i = start;
+	k = 0;
 	while (i <= end)
-	{
-		arr[i] = tmp_arr[i];
-		i++;
-	}
+		arr[i++] = tmp_arr[k++];
+	free(tmp_arr);
 }
 
-void	divide(int *arr, int start, int end, int *tmp_arr)
+void	merge_sort(int *arr, int start, int end)
 {
 	int	middle;
+
 	// halting condition
 	if (end - start <= 0)
 		return ;
 	
 	middle = (start + end) / 2;
+
 	// call function recursively to keep dividing
-	divide(arr, start, middle, tmp_arr);
-	divide(arr, middle + 1, end, tmp_arr);
+	merge_sort(arr, start, middle);
+	merge_sort(arr, middle + 1, end);
+	
 	// and then merge once you have the result
-	merge(arr, start, middle, end, tmp_arr);
-}
-
-void	merge_sort(int *arr, int start, int end)
-{
-	int	*tmp_arr;
-
-	tmp_arr = (int *)malloc(sizeof(int) * (end + 1 - start));
-	divide(arr, start, end, tmp_arr);
-	free(tmp_arr);
+	merge(arr, start, middle, end);
 }
